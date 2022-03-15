a93e1053b51cad8b5403c3da6c186843cf6491fa
“shashankvirat” <shashankcgowda81@gmail.com>
2022-03-15 07:49:44 +0000

    test script1

---
+++ t1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "enter the number to find the factorial"
+read num
+result=1
+while [ $num -gt 0 ]
+do
+	result=`expr $num \* $result`
+	num= `expr $num - 1`
+done
+echo " the factorial of given number is $result"
+
